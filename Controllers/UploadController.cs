7f36dc8f93efbdc67f222d22e7539c0f03d991cd
FTWinston <ftwinston@gmail.com>
2016-09-08 15:08:07 +0100

    Implemented (unsecure) upload and browsing of files

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,29 @@
+﻿using FileSite.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileSite.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(HttpPostedFileBase file)
+        {
+            var fileService = new FileService();
+            var name = Path.GetFileName(file.FileName);
+            var uri = fileService.Upload(name, file.InputStream);
+
+            return View("Success", uri);
+        }
+    }
+}
---
+++ Services/FileService.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace FileSite.Services
+{
+    public class FileService
+    {
+        public FileService()
+        {
+            Container = GetFileContainer();
+        }
+
+        public CloudBlobContainer Container { get; private set; }
+
+        private static CloudBlobContainer GetFileContainer()
+        {
+            var connectionString = CloudConfigurationManager.GetSetting("StorageConnectionString");
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            var containerName = CloudConfigurationManager.GetSetting("ContainerName");
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+
+            if (container.CreateIfNotExists())
+            {
+                var permissions = new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob };
+                container.SetPermissions(permissions);
+            }
+
+            return container;
+        }
+
+        public Uri Upload(string name, Stream data)
+        {
+            CloudBlockBlob blob = Container.GetBlockBlobReference(name);
+            blob.UploadFromStream(data);
+            return ConvertUri(blob.Uri);
+        }
+
+        public void Delete(string name)
+        {
+            CloudBlockBlob blob = Container.GetBlockBlobReference(name);
+            blob.Delete();
+        }
+
+        public IEnumerable<Uri> ListFiles()
+        {
+            // see "List the Blobs in a Container" here https://azure.microsoft.com/en-gb/documentation/articles/storage-dotnet-how-to-use-blobs/
+            foreach (IListBlobItem blob in Container.ListBlobs(null, true))
+            {
+                yield return ConvertUri(blob.Uri);
+            }
+        }
+        
+        private static Uri ConvertUri(Uri uri)
+        {
+            var customUrl = CloudConfigurationManager.GetSetting("CustomUrl");
+            if (string.IsNullOrEmpty(customUrl))
+                return uri;
+
+            var customUri = new Uri(customUrl);
+            return new Uri(string.Format("{0}://{1}{2}", customUri.Scheme, customUri.Host, uri.PathAndQuery), UriKind.Absolute);
+        }
+    }
+}
