667a63ad8dfd373da48086f590280feaa9ace2ca
FTWinston <ftwinston@gmail.com>
2016-09-08 17:01:41 +0100

    Removed some packages

--- Global.asax.cs
+++ Global.asax.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace FileSite
@@ -15,7 +14,6 @@ namespace FileSite
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
